607a720ce2a24480a41bec74e884b69986b0a5e2
habtehmh <habtehmh@gmail.com>
2016-12-06 09:25:32 -0500

    Step 2: Simple Offer 1. Offer class added to implement the new feature 2. Input gathering method modified to capture offer information 3. Check out method updated to check weather offer is available or not

---
+++ ShoppingCart/Offer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+
+    #region "Offer Class"
+    public class Offer
+    {
+        public static decimal CalculateOfferPrice(List<Item> items)
+        {
+            decimal _offerPrice = 0;
+            _offerPrice += OrangeOfferPrice(items);
+            _offerPrice += AppelOfferPrice(items);
+            return _offerPrice;
+        }
+        public static decimal OrangeOfferPrice(List<Item> items)
+        {
+            Orange _ornage = new Orange();
+            decimal _salePrice = 0;
+            _ornage.GetQuantity(items);
+            int _itemsRemain = 0;
+            for (int i = 1; i <= _ornage.Quantity; i += 3)
+            {
+
+                if (i == 1 && _itemsRemain == 0 && _ornage.Quantity > 2)
+                {
+                    _salePrice += _ornage.UnitPrice * 2;
+                    _itemsRemain = _ornage.Quantity - 3;
+                }
+                else
+                {
+                    if (_itemsRemain % 3 != 0)
+                    {
+                        _salePrice += _ornage.UnitPrice * (_itemsRemain % 3);
+                    }
+                    else
+                    {
+                        _salePrice += _ornage.UnitPrice * 2;
+                    }
+
+                    _itemsRemain = _ornage.Quantity - i;
+                }
+
+            }
+            return _salePrice;
+        }
+        public static decimal AppelOfferPrice(List<Item> items)
+        {
+            Apple _apple = new Apple();
+            decimal _salePrice = 0;
+            _apple.GetQuantity(items);
+            for (int i = 1; i <= _apple.Quantity; i += 2)
+            {
+                _salePrice += _apple.UnitPrice;
+            }
+            return _salePrice;
+        }
+    }
+    #endregion
+
+}
